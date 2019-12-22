fc0fbba6531ea7064c5dd3b314171e2d4a1b7cde
meikai <kai.meemken@seyes.org>
2019-12-22 11:16:12 +0100

    basically working versioning in function

--- functions/WritePreferenceToBucket.js
+++ functions/WritePreferenceToBucket.js
@@ -35,4 +35,27 @@ exports.helloPubSub = async (event, context) => {
   await save(jsonMessage, gcFile);
 };
 
+const helloLocal = async () => {
+  const preference = { id: 1122334, email: 'hans.wurst@peluda.de', v: 4 };
+  const jsonMessage = JSON.stringify(preference);
+  const gcFile = bucket.file(preference.id + '.pref');
+  let oldPreference;
+  try {
+    const file = await gcFile.download();
+    oldPreference = JSON.parse(file.toString());
+  } catch (e) {
+    console.error('Unable to determine old pref');
+  }
+  if (!oldPreference) {
+    await save(jsonMessage, gcFile);
+  } else {
+    if (preference.v === oldPreference.v + 1) {
+      await gcFile.copy(preference.id + '.pref.v' + oldPreference.v);
+      await save(jsonMessage, gcFile);
+    } else {
+      console.log('Conflict detected. Aborting');
+    }
+  }
+};
 
+helloLocal().then(() => console.log('done'));
