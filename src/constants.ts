4736d9d77ad715b794b3b0805c6372e8eef21a25
meikai <kai.meemken@seyes.org>
2019-12-24 14:47:40 +0100

    load and parse members w/ email

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const DGOB_URL = 'https://www.dgob.de/mv';
+export const DGOB_CREDENTIAL_ENDPOINT = DGOB_URL + '/anmeldung_ueberpruefung.php';
+export const DGOB_DATA_ENDPOINT = DGOB_URL + '/daten/anzeigen_mitglieder.php';
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,18 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 import { Member } from '../model/member.model';
+import { DGOB_CREDENTIAL_ENDPOINT, DGOB_DATA_ENDPOINT } from '../constants';
+import * as https from 'https';
+import { map, switchMap } from 'rxjs/operators';
+import { JSDOM } from 'jsdom';
 
 @Injectable()
 export class MemberService {
   members = new Map<string, Member>();
 
-  constructor() {
-    const hw: Member = {
-      id: '1234567',
-      email: 'hans.wurst@peluda.de',
-      firstname: 'Hans',
-      lastname: 'Wurst',
-      status: 'Vollmitglied',
-    };
-    this.members.set(hw.email, hw);
+  constructor(private http: HttpService) {
+    this.fetchMembersFromDGoB();
+    // tslint:disable-next-line:no-console
+    console.log('Started loading members from DGOB');
+  }
+
+  async fetchMembersFromDGoB() {
+    const agent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+    const reqSession = await this.http
+      .post(
+        DGOB_CREDENTIAL_ENDPOINT,
+        'Name=bgov&Passwort=' + process.env.BGOV_PW,
+        {
+          auth: { username: 'bgovmv', password: process.env.BGOV_MV_PW },
+          responseType: 'text',
+          httpsAgent: agent,
+        },
+      )
+      .pipe(
+        map(req => req.headers['set-cookie']),
+        map(cookies => cookies.find(cookie => cookie.startsWith('PHPSESSID'))),
+        switchMap(cookie =>
+          this.http.post(DGOB_DATA_ENDPOINT, 'LVID=09', {
+            auth: { username: 'bgovmv', password: process.env.BGOV_MV_PW },
+            headers: { Cookie: cookie },
+            httpsAgent: agent,
+          }),
+        ),
+      )
+      .toPromise();
+    this.parseMembers(reqSession.data).forEach(member => {
+      const emailRegex = new RegExp(
+        '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])',
+      );
+      if (emailRegex.test(member.email)) {
+        this.members.set(member.email, member);
+      }
+    });
+    // tslint:disable-next-line:no-console
+    console.log(`Loaded ${this.members.size} members w/emails from DGOB`);
+  }
+
+  parseMembers(membersHtml) {
+    const dom = new JSDOM(membersHtml);
+    const $ = require('jquery')(dom.window);
+    const rows = [...$('tr')];
+    return rows.map(row => {
+      return {
+        id: row.querySelector('td:nth-child(2)').textContent,
+        firstname: row.querySelector('td:nth-child(6)').textContent,
+        lastname: row.querySelector('td:nth-child(5)').textContent,
+        status: row.querySelector('td:nth-child(10)').textContent,
+        email: row.querySelector('td:nth-child(14)').textContent,
+      };
+    });
   }
 }
