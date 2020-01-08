b0ec54e386a5df666d918fbbec605c6f3cdccca4
meikai <meikai@seyes.org>
2020-01-08 11:03:24 +0100

    read config w/ secrets from protected bucket

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Request, Response } from 'express';
+import { Storage } from '@google-cloud/storage';
+import { parse } from 'dotenv';
 
 async function bootstrap() {
+  const storage = new Storage();
+  const bucket = storage.bucket('bgov-web-config');
+  const envFile = bucket.file('bgov-web.env');
+  const file = await envFile.download();
+  const parsed = parse(Buffer.concat(file));
+  Object.keys(parsed).forEach(key => {
+    if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+      process.env[key] = parsed[key];
+    } else {
+      console.error(
+        `"${key}" is already defined in \`process.env\` and will not be overwritten`,
+      );
+    }
+  });
   const app = await NestFactory.create(AppModule);
   app.use((req: Request, res: Response, next) => {
     res.set('Access-Control-Allow-Origin', '*');
--- src/preference/preference.service.ts
+++ src/preference/preference.service.ts
@@ -4,10 +4,17 @@ import { Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class PreferenceService {
+  bucket;
   preferences = new Map<string, Preference>();
 
-  loadFromBucket() {
+  constructor() {
     const storage = new Storage();
-    // storage.bucket('bgov-web-preferences').upload()
+    this.bucket = storage.bucket('bgov-web-preferences');
+    this.loadFromBucket();
+  }
+
+  async loadFromBucket() {
+    const [files] = await this.bucket.getFiles();
+    files.forEach(file => console.log(file));
   }
 }
