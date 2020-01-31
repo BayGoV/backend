6180c230a62928d9c161aaab3f51d1a724426cc1
meikai <kai.meemken@seyes.org>
2020-01-31 19:53:05 +0100

    added contact to meetup

--- src/model/meetup.model.ts
+++ src/model/meetup.model.ts
@@ -1,4 +1,4 @@
-import { VersionedDataTransferObject } from './versioned-data-transfer-object';
+import {VersionedDataTransferObject} from './versioned-data-transfer-object';
 
 class ScopedField {
   value: string;
@@ -7,11 +7,27 @@ class ScopedField {
 
 // tslint:disable-next-line:max-classes-per-file
 export class Meetup extends VersionedDataTransferObject {
+  constructor() {
+    super();
+    Object.assign(this, {
+      id: Date.now().toString(),
+      name: '',
+      address: {value: '', scope: 'public'},
+      frequency: {value: '', scope: 'public'},
+      email: {value: '', scope: 'public'},
+      phone: {value: '', scope: 'public'},
+      contact: {value: '', scope: 'public'},
+      scope: 'private',
+      s: 'New',
+    });
+  }
+
   memberId: string;
   name: string;
   address: ScopedField;
   frequency: ScopedField;
-  email?: ScopedField;
-  phone?: ScopedField;
+  email: ScopedField;
+  phone: ScopedField;
+  contact: ScopedField;
   scope: 'public' | 'internal' | 'private';
 }
