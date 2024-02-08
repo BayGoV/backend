21282a7d21e9091710f8b5e490c994952f441765
Kai Meemken <Kai Meemken>
2024-02-08 13:26:03 +0100

    Added shorter Websocket Ping Timeouts and a global timeout after 5 min inactivity

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -2,18 +2,22 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { MemberService } from './member/member.service';
 import { JwtStrategy } from './jwt.strategy';
 import { verify } from 'jsonwebtoken';
-import { Subject } from 'rxjs';
+import { interval, Subject } from 'rxjs';
 import { SocketMessage } from './model/socketMessage.model';
+import { map } from 'rxjs/operators';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
+@WebSocketGateway({ pingInterval: 5000, pingTimeout: 10000 })
 export class EventsGateway {
   private subscriptions = new Map<string, Subject<SocketMessage>>();
+  private lastConnectionTimes = new Map<string, number>();
   private clients = [];
 
   constructor(
     private memberService: MemberService,
     private jwtStrategy: JwtStrategy,
-  ) {}
+  ) {
+  }
 
   @SubscribeMessage('subscribe')
   async handleMessage(client: any, payload: any) {
@@ -29,10 +33,20 @@ export class EventsGateway {
     }
     if (isNew || !this.clients.includes(client.id)) {
       subscription.subscribe(message => {
+        this.lastConnectionTimes.set(member.id, Date.now());
         client.emit('update', message);
       });
       this.clients.push(client.id);
     }
+    this.lastConnectionTimes.set(member.id, Date.now());
+    const timeout = 60000;
+    interval(5 * timeout).pipe(map(_ => {
+      const lastConnected = this.lastConnectionTimes.get(member.id);
+      if (Date.now() - lastConnected > timeout) {
+        Logger.log(`Disconnecting ${member.email} after 5min timeout`);
+        client.disconnect();
+      }
+    })).subscribe();
     return `Subscribed for ${member.id}`;
   }
 
